98e92074b42d96eb2d186093e36b790453dc051f
vtthoapl <vtthoapl@gmail.com>
2023-05-29 12:31:26 +0300

    scss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,12 +18,14 @@ const Header = () => {
           <NavLink to="admins" className='nav-link'>Admins</NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+          <button className='btn-login'>Login</button>
+          <button className='btn-signup'>Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item >Log-in</NavDropdown.Item>
               <NavDropdown.Item >Log-out</NavDropdown.Item>
               <NavDropdown.Item >Profile</NavDropdown.Item>
               <NavDropdown.Divider />
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
