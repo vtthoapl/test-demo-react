6b92f3dc1c553601e1d5dd3da9d830d828532652
vtthoapl <vtthoapl@gmail.com>
2023-06-05 16:16:00 +0300

    scss

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,17 +1,20 @@
-import ModalCreateUser from "./ModalCreateUser";
-import './ManageUser.scss'
+import ModalCreateUser from './ModalCreateUser';
+import './ManageUser.scss';
 
 const ManageUser = (props) => {
   return (
     <div className="manage-user-container">
-      <div className="title">manage user</div>
-      <div className="user-content">
-        <div>
-          <button>Add new user</button>
-        </div>
-        <div>table user</div>
+      <div className="title">
+      Manage user
       </div>
-        <ModalCreateUser />
+      <div className="user-content">
+        <div className="btn-add-new">
+          <button className='btn btn-primary'>Add new user</button>
+        </div>
+        <div className="table-users-container">table user
+        </div>
+      <ModalCreateUser />
+      </div>
     </div>
   );
 };
